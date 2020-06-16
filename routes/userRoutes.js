3aacf20939480f3a789a7a7061c0ee659d14aa35
unknown <evanshango>
2020-06-16 19:50:47 +0300

    handle creating account using google oauth2 and saving user ingo in database

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,19 @@
 const router = require('express-promise-router')()
+const passport = require('passport')
+const passportConfig = require('../passport')
 
 const {validateBody, schemas} = require('../helpers/inputValidators')
-const {signup, signin, secrete} = require('../controllers/userController')
+const {signup, signin, googleOAuth, secrete} = require('../controllers/userController')
+const passportSignIn = passport.authenticate('local', {session: false})
+const passportJwt = passport.authenticate('jwt', {session: false})
+const passportGoogle = passport.authenticate('googleToken', {session: false})
 
 router.post('/signup', validateBody(schemas.authSchema), signup)
-router.post('/signin', signin)
-router.get('/secret', secrete)
+router.post('/signin', validateBody(schemas.authSchema), passportSignIn, signin)
+//OAuth route
+router.post('/oauth/google', passportGoogle, googleOAuth)
+
+//Sample route that requires authentication
+router.get('/secret', passportJwt, secrete)
 
 module.exports = router
